bb80c76d608cd9b17988502ebd7be0682d3dd828
jafarkhan7 <jafarkhan178@gmail.com>
2024-10-04 15:24:22 +0400

    Initial Commit

---
+++ PropertyFinder/PropertyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyFinderApp.swift
+//  PropertyFinder
+//
+//  Created by Jafar on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PropertyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
